dc14e49b5bb451344cf03a02c7e22535d1cb7ecd
Robert Rothenberg <rrwo@cpan.org>
2018-11-18 11:47:19 +0000

    Commit Changes and bump $VERSION

--- lib/Types/SQL.pm
+++ lib/Types/SQL.pm
@@ -22,7 +22,7 @@ use namespace::autoclean;
 
 # ABSTRACT: a library of SQL types
 
-our $VERSION = 'v0.3.1';
+our $VERSION = 'v0.3.2';
 
 =for Pod::Coverage VERSION
 
--- lib/Types/SQL/Util.pm
+++ lib/Types/SQL/Util.pm
@@ -8,7 +8,7 @@ use Exporter qw/ import /;
 use PerlX::Maybe;
 use Safe::Isa 1.000008 qw/ $_isa $_call_if_can /;
 
-our $VERSION = 'v0.3.1';
+our $VERSION = 'v0.3.2';
 
 # RECOMMEND PREREQ: PerlX::Maybe::XS
 
